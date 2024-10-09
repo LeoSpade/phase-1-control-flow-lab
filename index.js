2149f60f920cb68d1de51d1f9e99b2c179e914c6
thornm <atmaystar@gmail.com>
2024-10-09 13:54:25 -0400

    commit review control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  if (distance < 400) {
+    return "This one is on me!"
+  }
+  else if (distance < 2000) {
+    return "That will be twenty bucks."
+  }
+  else if (distance < 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (distance > 2500) {
+    return "No can do."
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+    return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+
+  let charm;
+
+  switch (tip) {
+    case "generous":
+      charm = "Thank you so much.";
+      break;
+    case "not as generous":
+      charm = "Thank you.";
+      break;
+    default:
+      charm = "Bye."
+      break;
+  }
+  return charm
 }
+
+// const ternaryCheckCity = "NYC": "Ok, sounds good."
